e92632e6539e7fda7b3132bde09bfc52c4709a07
aume1 <aume.1@themetroschool.org>
2014-01-23 08:36:26 -0500

    fixed error

--- SimpleTemplate/drive.cpp
+++ SimpleTemplate/drive.cpp
@@ -101,7 +101,7 @@ void Drive::Disable(){
 void Drive::SetMecanumRLStrafe( double r, double l, double strafe ){
 
 	driverX = strafe;
-	driverY = ( r + l ) / 2) * ( r + l ) / 2);
+	driverY = (( r + l ) / 2) * (( r + l ) / 2);
 	driverTurn = (( l - r ) / 2) * (( l - r ) / 2);
 
 }
