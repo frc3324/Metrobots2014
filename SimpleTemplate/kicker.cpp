c0ca9c4abdd06b03b3779997f8ebc8203db630df
aume1 <aume.1@themetroschool.org>
2014-01-29 18:29:31 -0500

    added kicker.h and kicker.cpp

---
+++ SimpleTemplate/kicker.cpp
@@ -0,0 +1,21 @@
+#include "kicker.h"
+#include <Math.h>
+
+Kicker::Kicker( SpeedController *kicker1_, SpeedController *kicker2_);
+
+		kicker1 = kicker1_;
+		kicker2 = kicker2_;
+		
+}
+
+void Kicker::PullBackKicker() {
+		
+		
+		
+}
+
+void Kicker::KickBall() {
+		
+		
+		
+}
---
+++ SimpleTemplate/kicker.h
@@ -0,0 +1,20 @@
+#ifndef KICKER_H
+#define KICKER_H
+
+#include "WPILib.h"
+#include <Math.h>
+#include "util/metropidcontroller.h"
+
+class Kicker {
+
+	public:
+		Kicker( SpeedController *kicker1, SpeedController *kicker2);
+		~Kicker(){};
+		void PullBackKicker();
+		void KickBall();
+		
+		SpeedController kicker1;
+		SpeedController kicker2;
+};
+
+#endif
